d6ec1569f1209203eff87bb11deeff74741ee2cb
Fungai <fungai@vividtheory.com>
2022-08-09 14:25:33 -0600

    Spacing

--- Question_1/Question_1.ts
+++ Question_1/Question_1.ts
@@ -1,15 +1,27 @@
+
+
+
 //f: A -> [A] -> [A]
 const push = <A>(a: A, l: Array<A>): Array<A> =>
             {   l[l.length] = a
                 return l
             };
 
+
+
+
 //f: Number -> Number -> [A] -> [A]
 const slice = <A>(num1: number, num2: number, l: Array<A>): Array<A> => l.slice(num1, num2);
 const splice = <A>(num1: number, num2: number, l: Array<A>): Array<A> => l.splice(num1, num2);
 
+
+
+
 //f: [String] -> {String: any} -> {String: any}
 // const c =
+
+
+
 
 //f: [A] -> [B] -> [[A,B]]
 const to2DArray = <A, B>(a: Array<A>, b: Array<B>): [A, B][] => {
