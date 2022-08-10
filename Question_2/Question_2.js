9b8d2369f73be5ffbfdd78e40eb0622f85c49ac1
Fungai <fungai@vividtheory.com>
2022-08-10 17:29:35 -0600

    Uncommenting out code

--- Question_2/Question_2.js
+++ Question_2/Question_2.js
@@ -1,52 +1,27 @@
+// import { pipe } from "fp-ts/function";
 import fetch from "node-fetch";
-// interface Entity {
-//   id: string;
-// }
-// interface Post extends Entity {
-//   __tag: "post";
-//   userId: number;
-//   title: string;
-//   body: string;
-// }
-// interface Comment extends Entity {
-//   __tag: "comment";
-//   postId: number;
-//   name: string;
-//   email: string;
-//   body: string;
-// }
-// type ApiResponse<T extends Entity> =
-//   | { status: "success", data: T[] }
-//   | { status: "error", error: string };
-// const fetchMockData = (typeOfData: "comments" | "posts") => {
-//   if(typeOfData === "posts"){
-//     fetch('https://jsonplaceholder.typicode.com/posts')
-//     .then((response) => response.json())
-//     .then((json) => console.log(json));
-//   }
-//   if(typeOfData === "comments"){
-//     fetch('https://jsonplaceholder.typicode.com/comments')
-//     .then((response) => response.json())
-//     .then((json) => console.log(json));
-//   }
-// }
-// const fetchComments = () => {
+async function fetchMockData(typeOfData) {
+    if (typeOfData === "posts") {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        return response;
+    }
+    if (typeOfData === "comments") {
+        const response = await fetch('https://jsonplaceholder.typicode.com/comments');
+        return response;
+    }
+}
+// async function fetchComments (){
 //   pipe("comments", fetchMockData)
 // }
-// const fetchPosts = () => {
+// async function fetchPosts (){
 //   pipe("posts", fetchMockData)
 // }
-// const isSuccess = (api: ApiResponse<Comment>|ApiResponse<Post>): api is { status: "success", data: Comment[]} | { status: "success", data: Post[]} => api.status === "success";
-// const match = <T2>(
-//   someSuccess: (t: Comment[]|Post[]) => T2,
-//   someError: (error: string) => T2
-// ) => (api: ApiResponse<Comment>|ApiResponse<Post>): T2 =>{
-//   if(isSuccess(api)) return someSuccess(api.data);
-//   else return someError(api.error);
-// }
-async function getData() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/comments');
-    return response.json();
-}
-const data = await getData();
+const isSuccess = (api) => api.status === "success";
+const match = (someSuccess, someError) => (api) => {
+    if (isSuccess(api))
+        return someSuccess(api.data);
+    else
+        return someError(api.error);
+};
+const data = await fetchMockData("comments");
 console.log({ data });
