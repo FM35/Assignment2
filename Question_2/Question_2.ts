fd1b9d2089317401c7239c239b6e2c1fb5bc0260
fungai <fungaimawoyo@gmail.com>
2022-08-12 07:32:24 -0600

    Formatting

--- Question_2/Question_2.ts
+++ Question_2/Question_2.ts
@@ -1,4 +1,4 @@
-import fetch from "node-fetch"
+import fetch from "node-fetch";
 
 interface Entity {
   id: string;
@@ -20,46 +20,51 @@ interface Comment extends Entity {
 }
 
 type ApiResponse<T extends Entity> =
-  | { status: "success", data: T[] }
-  | { status: "error", error: string };
+  | { status: "success"; data: T[] }
+  | { status: "error"; error: string };
 
-async function fetchMockData (typeOfData: "comments" | "posts"){
-  if(typeOfData === "posts"){
-
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-
-    return response;
-  }
-
-  if(typeOfData === "comments"){
-
-    const response = await fetch('https://jsonplaceholder.typicode.com/comments');
+async function fetchMockData(typeOfData: "comments" | "posts") {
+  if (typeOfData === "posts") {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
 
     return response;
   }
+
+  if (typeOfData === "comments") {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    );
+
+    return response;
+  }
 }
 
-async function fetchComments (){
-  fetchMockData("comments");
+async function fetchComments() {
+  return fetchMockData("comments");
 }
 
-async function fetchPosts (){
-  fetchMockData("posts");
+async function fetchPosts() {
+  return fetchMockData("posts");
 }
 
-const isSuccess = (api: ApiResponse<Comment>|ApiResponse<Post>): api is { status: "success", data: Comment[]} | { status: "success", data: Post[]} => api.status === "success";
+const isSuccess = (
+  api: ApiResponse<Comment> | ApiResponse<Post>
+): api is
+  | { status: "success"; data: Comment[] }
+  | { status: "success"; data: Post[] } => api.status === "success";
 
-const match = <T2>(
-  someSuccess: (t: Comment[]|Post[]) => T2,
-  someError: (error: string) => T2
-) => (api: ApiResponse<Comment>|ApiResponse<Post>): T2 =>{
-  if(isSuccess(api)) return someSuccess(api.data);
-  else return someError(api.error);
-}
+const match =
+  <T2>(
+    someSuccess: (t: Comment[] | Post[]) => T2,
+    someError: (error: string) => T2
+  ) =>
+  (api: ApiResponse<Comment> | ApiResponse<Post>): T2 => {
+    if (isSuccess(api)) return someSuccess(api.data);
+    else return someError(api.error);
+  };
 
+const data = await fetchComments();
 
-const data = await fetchMockData("comments");
-
-console.log({ data })
+console.log({ data });
 
 //Post id from comments relates to id of post
